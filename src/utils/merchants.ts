b9dc5266032315e93ef2122305dc2c21285fa7c0
Choo Chen Wei <chenwei.choo@ufinity.com>
2024-06-04 15:28:27 +0800

    [CCW] add merchantClaimUrl helper

--- src/utils/merchants.ts
+++ src/utils/merchants.ts
@@ -1,3 +1,5 @@
+import { Store } from "../constants";
+
 export const merchants = [
   {
     name: 'Fairprice',
@@ -12,3 +14,17 @@ export const merchants = [
     url: 'https://shopee.sg'
   }
 ];
+
+
+export const merchantClaimUrl = (store:Store,orderId:string) =>{
+  switch(store){
+    case Store.FAIRPRICE:
+      return `https://www.fairprice.com.sg/orders/${orderId}`;
+    case Store.SHENGSIONG:
+      return `https://www.shengsiong.com.sg/orders/${orderId}`;
+    case Store.SHOPEE:
+      return `https://www.shopee.com.sg/orders/${orderId}`;
+    default:
+      return "";
+  }
+}
