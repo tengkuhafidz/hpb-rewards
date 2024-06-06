e46a102016a27264c4fa291533e58fcce1753339
Choo Chen Wei <chenwei.choo@ufinity.com>
2024-06-06 15:12:51 +0800

    [MISC][CCW] add FP claim logic

---
+++ src/content-scripts/claim/fairprice.ts
@@ -0,0 +1,49 @@
+import { MerchantPoints, Store } from '../../constants';
+import { fetchData, saveData } from '../../utils/storage';
+
+export const processFairPriceClaim = async (currPage: string) => {
+  const values = currPage.split('/');
+  const orderId = values[values.length - 1];
+
+  const fairPriceOrderIdSelector = `Delivery ID: ORD${orderId}`;
+
+  const orderIdElementExist =
+    document.documentElement.textContent ||
+    document.documentElement.innerText.indexOf(fairPriceOrderIdSelector) > -1;
+  const completedElementExist =
+    document.documentElement.innerText.indexOf('Order delivered!') > -1;
+
+  if (!orderIdElementExist) {
+    return;
+  }
+
+  if (!completedElementExist) {
+    alert('The order is still being process...cannot be claimed');
+    return;
+  }
+
+  const response = await fetchData('userId123');
+  const data: MerchantPoints = response['userId123'];
+
+  if (data) {
+    const fairPriceOrders = data[Store.FAIRPRICE];
+    const currentOrderIndex = fairPriceOrders.findIndex(
+      order => order.orderId === orderId
+    );
+
+    if (currentOrderIndex >= 0) {
+      const currentOrder = fairPriceOrders[currentOrderIndex];
+      if (currentOrder.isClaimed) {
+        alert(`This ${orderId} id is already claimed!`);
+      } else {
+        fairPriceOrders[currentOrderIndex] = {
+          ...currentOrder,
+          isClaimed: true
+        };
+
+        saveData(data, 'userId123');
+        alert(`You have claimed ${currentOrder.points} HDB Points`);
+      }
+    }
+  }
+};
--- src/content.ts
+++ src/content.ts
@@ -2,16 +2,20 @@ import browser from 'webextension-polyfill';
 import { highlightFairpriceHcsItems } from './content-scripts/highlight/fairprice';
 import { observer } from './content-scripts/confirmation-modal';
 import { MERCHANTS } from './utils/merchants';
+import { processFairPriceClaim } from './content-scripts/claim/fairprice';
 
 // Observe in checkout page to inject the mock Place order button when it appears in the DOM
 observer.observe(document, { childList: true, subtree: true });
 
 browser.runtime.onMessage.addListener(async (obj, sender, response) => {
-  const { type } = obj;
+  const { type,currPage } = obj;
 
+  
+	
 	// TODO: add support for other merchants
 	switch (type) {
 		case MERCHANTS.FAIRPRICE:
+      await processFairPriceClaim(currPage);
 			highlightFairpriceHcsItems();
 			break;
 	}
